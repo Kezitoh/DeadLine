e39718d09f4f002283b85831017bc2ce08f8582a
Alex Deb Duq <alejandrodebduq@gmail.com>
2024-04-25 17:43:29 +0200

    Estado inicial del proyecto actualizado
    actualizados index.html y main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let game;
+
+let wfConfig = {
+    active: function () {
+        startGame();
+    },
+    google: {
+        families: ['Titan One']
+    },
+    custom: {
+    }
+};
+
+function startGame() {
+    game = new Phaser.Game(800,800, Phaser.CANVAS, 'gameScreen');
+    // Welcome Screen
+    //game.state.add('welcome', initialState);
+    // About Screen (instructions)
+    //game.state.add('about', aboutState);
+    // Config Screen
+    //game.state.add('config', configState);
+    // Play Screen
+    //game.state.add('play', playState);
+    
+    //game.state.start('welcome');
+}
