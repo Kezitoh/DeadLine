155d702a88247941b8cde1bdc0ce3a089d65e9c0
stxvsan <stevenenrique35@gmail.com>
2024-05-30 22:41:55 +0200

    fg
    fhgj

--- js/instructions.js
+++ js/instructions.js
@@ -8,6 +8,7 @@ function loadInstructionsAssets(){
     game.load.image('homeBtn', 'assets/UI/cancelButton.png');
     game.load.image('forwardBtn', 'assets/UI/forwardButton.png');
     game.load.image('backBtn', 'assets/UI/backButton.png');
+    game.load.image('wasdImagen', '../assets/UI/WASD.png');
 }
 
 const SLIDE_TIME = 15000;
@@ -46,7 +47,7 @@ function showSlide1(){
 
     let slide1Text = 'Use WASD or ARROWS to move the player character.\n';
     slide1Text += 'AIM with the MOUSE and LEFT CLICK to SHOOT\n';
-    slide1Text += 'SHOOT the enemies to gain points!';
+    slide1Text += 'SHOOT the enemies to gain points!\n \n \n \n \n \n \n In a simple way, to win you need the gems that the robots drop';
 
     let slide1TextBox = game.add.text(game.canvas.width / 2, game.canvas.height / 5, slide1Text, slideTextStyle);
     slide1TextBox.anchor.setTo(0.5, 0);
@@ -57,6 +58,9 @@ function showSlide1(){
     let backBtn = game.add.button(game.canvas.width / 12.5, game.canvas.height / 1.25, 'homeBtn', () => {backBtnPress(slideNum)});
     backBtn.scale.setTo(1.5);
 
+    let wasdImage = game.add.sprite(game.canvas.width / 6, game.canvas.height/2 , 'wasdImagen');
+    wasdImage.anchor.set(0.5);
+    wasdImage.scale.setTo(0.25);
     animacionEntrada();
 }
 
@@ -65,7 +69,7 @@ function showSlide2(){
     slideNum = 2;
     timer = game.time.events.add(SLIDE_TIME, () => {backBtnPress(slideNum)}, this);
 
-    let slide2Text = 'TEXT FOR THE SLIDE 2';
+    let slide2Text = "You can buy weapons in the safe zone \nbut don't stay too comfortable there, \nsedentary lifestyle is Lethal ;3 \nYou can equip the weapons you buy by pressing the Q key \nand with the same key change it for another one.\n \n To win is simple. The only thing you have to do is kill everyone. \nDON'T worry about them, they no longer have life, \nwe make sure of them :)";
     let slide2TextBox = game.add.text(game.canvas.width / 2, game.canvas.height / 5, slide2Text, slideTextStyle);
     slide2TextBox.anchor.setTo(0.5,0);
     slide2TextBox.setShadow(5,5);
