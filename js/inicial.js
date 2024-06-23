897f2cfaf8d48498c7369943c497062f998df71c
stxvsan <stevenenrique35@gmail.com>
2024-06-24 00:23:32 +0200

    a
    a

--- js/inicial.js
+++ js/inicial.js
@@ -22,9 +22,26 @@ function trushInitial() {
         font: '90pt Titan One',
         fill: '#F9F9F9'
     };
+    let textTitleInitial2 = ' (Click on the game screen to continue...) ';
+    let styleTitleInitial2 = {
+        font: '15pt Titan One',
+        fill: '#F9F9F9'
+    };
+
 
     let te = game.add.text(game.canvas.width / 2, game.canvas.height / 2, textTitleInitial, styleTitleInitial);
     te.anchor.setTo(0.5, 0.5);
+
+
+    let tex = game.add.text(game.canvas.width / 2, te.bottom + 5, textTitleInitial2, styleTitleInitial2);
+    tex.anchor.setTo(0.5, 0.5);
+    let botonesTween2 = game.add.tween(tex).to({
+        alpha: 0
+    }, 1000, Phaser.Easing.Cubic.InOut).to({
+        alpha: 1
+    }, 1000, Phaser.Easing.Cubic.InOut);
+    botonesTween2.loop(true);
+    botonesTween2.start();
 
     te.inputEnabled = true;
 
@@ -35,16 +52,21 @@ function trushInitial() {
     }, 1000, Phaser.Easing.Cubic.Out);
     botonesTween.onComplete.add(() => { game.state.start('welcome');})
 
+
     // Agregar evento de clic
     te.events.onInputDown.add(() => {
         botonesTween.start();
     });
 
+    game.canvas.addEventListener('pointerdown', () => { botonesTween.start();});
+
     te.events.onInputOver.add(() => {
-        te.fill = '#FF0000'; // Cambia el color a rojo cuando el ratón está encima
+        te.fill = '#00FF13'; // Cambia el color a rojo cuando el ratón está encima, fui obligado
     });
 
     te.events.onInputOut.add(() => {
         te.fill = '#F9F9F9'; // Cambia el color de nuevo al original cuando el ratón sale
     });
+
+
 }
